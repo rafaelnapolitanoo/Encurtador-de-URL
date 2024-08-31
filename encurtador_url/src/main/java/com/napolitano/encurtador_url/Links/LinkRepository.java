2c7aadf3573166ad05e29808305debd0dfef120c
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-31 08:54:09 -0300

    feat: created LinkService

--- encurtador_url/src/main/java/com/napolitano/encurtador_url/Links/LinkRepository.java
+++ encurtador_url/src/main/java/com/napolitano/encurtador_url/Links/LinkRepository.java
@@ -3,4 +3,7 @@ package com.napolitano.encurtador_url.Links;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LinkRepository extends JpaRepository<Link , Long> {
+
+    Link findByLongUrl(String shortenedUrl);
+
 }
