9383b1610fa45bbcf9daa529b3d6eeaf2ce3b172
rafaelnapolitano <rafaelnv.prof@gmail.com>
2024-08-31 08:54:16 -0300

    feat: created LinkService

---
+++ encurtador_url/src/main/java/com/napolitano/encurtador_url/Links/LinkService.java
@@ -0,0 +1,51 @@
+package com.napolitano.encurtador_url.Links;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+
+/*
+    O encurtador de URLs recebe uma URL longa como parâmetro inicial.
+    O encurtamento será composto por um mínimo de 05 e um máximo de 10 caracteres.
+    Apenas letras e números são permitidos no encurtamento.
+    A URL encurtada será salva no banco de dados com um prazo de validade (você pode escolher a duração desejada).
+    Ao receber uma chamada para a URL encurtada https://xxx.com/DXB6V, você deve fazer o redirecionamento para a URL original salva no banco de dados.
+    Caso a URL não seja encontrada no banco, retorne o código de status HTTP 404 (Not Found).
+*/
+
+@Service
+public class LinkService {
+
+
+    private LinkRepository linkRepository;
+    public LinkService(LinkRepository linkRepository){
+        this.linkRepository= linkRepository;
+    }
+
+    // gerar uma url aleatoria
+    public String generateSortedUrl() {
+        return RandomStringUtils.randomAlphanumeric(5,10);
+    }
+
+    // encurtar url
+    public Link encurtarUrl(String longUrl) {
+        Link link = new Link();
+        link.setLongUrl(longUrl);
+        link.setShortenedUrl(generateSortedUrl());
+        link.setUrlCreatedIn(LocalDateTime.now());
+        link.setQrCodeUrl("QR code indisponivel no momento");
+
+        return linkRepository.save(link);
+    }
+
+    public Link obterUrlOriginal(String shortenedUrl) {
+        try {
+            return  linkRepository.findByLongUrl(shortenedUrl);
+        } catch (Exception exception){
+            throw new RuntimeException("Url not found", exception);
+        }
+
+    }
+}
